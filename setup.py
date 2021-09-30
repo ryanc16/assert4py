a0739a710dc91c88ab266476112a802c45ecc0b3
ryanc16 <rconklin1994@yahoo.com>
2021-09-30 03:38:31 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "assert4py",
+    version = "1.0.0-alpha",
+    author = "ryanc16",
+    author_email = "",
+    description = "A unit testing suite library for python",
+    license = "MIT",
+    keywords = "tests, testing, test suite, test runner, unit test, assert, mock",
+    url = "https://github.com/ryanc16/assert4py",
+    packages=['assert4py'],
+    package_dir={'assert4py': 'assert4py'},
+    long_description="A unit testing suite, test runner, and assertion library for python",
+)
